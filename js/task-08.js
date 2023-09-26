474bb25ca2d77ddd2da783ce60147b1f362e8d38
Anton <a.chertok@akatech.com.ua>
2023-09-26 11:40:21 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const form = e.target;
+  const { email, password } = form.elements;
+
+  if (email.value === "" || password.value === "") {
+    alert("All fields have to be filled!");
+    return;
+  }
+
+  const result = { email: email.value, password: password.value };
+  console.log(result);
+
+  form.reset();
+});
